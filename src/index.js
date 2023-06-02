2fa7fbc7f52c767b954c220d4454822c602024d3
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-06-03 00:40:11 +0300

    task3

--- src/index.js
+++ src/index.js
@@ -10,17 +10,21 @@ const refs = {
 const API = new filmAPI();
 API.getCategoriesGenres().then(genresData => {
   const genreMap = createGenreMap(genresData);
+  // console.log(genreMap);
   API.getCategories().then(res => {
+    // console.log(res, genreMap);
     markupFilms(res, genreMap);
     // initPagination();
   });
 });
 
 function createGenreMap(genresData) {
-  return genresData.reduce((acc, genre) => {
+  const accGenresData = genresData.reduce((acc, genre) => {
     acc[genre.id] = genre.name;
     return acc;
   }, {});
+  // console.log(acc);
+  return accGenresData;
 }
 
 function markupFilms(data, genreMap) {
@@ -32,23 +36,29 @@ function markupFilms(data, genreMap) {
         original_name,
         genre_ids,
         release_date,
+        first_air_date,
       }) => {
         const url = `https://image.tmdb.org/t/p/original${poster_path}`;
         const year =
           typeof release_date !== 'undefined'
             ? release_date.split('-')[0]
-            : 2023;
-        let genreNames = genre_ids.map(genreId => genreMap[genreId]);
+            : first_air_date.split('-')[0];
+        let genreNames = genre_ids.map(genreId => { 
+          if(!genreMap[genreId]){
+            return 'Film'
+          }
+          return genreMap[genreId]});
         if (genreNames.length > 3) {
-          genreNames = genreNames.slice(0, 3);
+           genreNames = genreNames.slice(0, 3);
+           return;
         }
+        // console.log(genreNames)
         return `<li>
-        <img src="${url}" alt="${
-          original_name || original_title
-        }" width="395" height="574" class="search_film_img">
+        <img src="${url}" alt="${ original_name || original_title}"
+         width="395" height="574" class="search_film_img"/>
         <div class="search_film_wrap">
           <p class="search_film_title">${original_name || original_title}</p>
-          <p class="search_film_genre">${genreNames || ''} | ${year}</p>
+          <p class="search_film_genre">${genreNames} | ${year}</p>
         </div>
       </li>`;
       }
