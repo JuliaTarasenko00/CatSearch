c3cae113a362dc175e626bf12d6f825cd32c0a13
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-06-04 13:33:00 +0300

    task4

--- src/js/API.js
+++ src/js/API.js
@@ -35,14 +35,14 @@ export class filmAPI {
       console.log(err);
     }
   }
-  async getCategoriesQuery(q) {
+  async getCategoriesQuery(query) {
     const endpoint = 'search/movie';
     const { data } = await axios.get(
-      `${BASE_URL}/${endpoint}?api_key=${API_KEY}&query=${q}&page=${this.page}&language=en-US`
+      `${BASE_URL}/${endpoint}?api_key=${API_KEY}&query=${query}&page=${this.page}&language=en-US`
     );
     // console.log(data);
     try {
-      return data.genres;
+      return data.results;
     } catch (err) {
       console.log(err);
     }
