20055ddc1dcf8b856266a6bad7d8d3cd16ca63d8
YaroslavZamrii <119421294+YaroslavZamrii@users.noreply.github.com>
2023-06-07 16:02:26 +0300

    summary

--- src/favorite.js
+++ src/favorite.js
@@ -1,7 +1,18 @@
+import { filmAPI } from './js/API';
+const API = new filmAPI();
+
 const KEY_FAVORITE = 'favorite';
 
 const list = document.querySelector('.js_list');
 const favorite = JSON.parse(localStorage.getItem(KEY_FAVORITE)) ?? [];
+
+API.getCategoriesGenres()
+  .then(res => {
+    res.forEach(({ id, name }) => {
+      categories[id] = name;
+    });
+  })
+  .catch(Error);
 
 function markupFilm(favorite) {
   const markup = favorite
@@ -20,14 +31,14 @@ function markupFilm(favorite) {
           ? `https://image.tmdb.org/t/p/original${poster_path}`
           : 'https://www.tgv.com.my/assets/images/404/movie-poster.jpg';
         const year = yearsFilm(release_date, first_air_date);
-        // let genre = categoriesFilms(genre_ids);
+        let genre = categoriesFilms(genre_ids);
         return `<li class="search_film_img_wrap" data-id='${id}'>
 <img src="${url}" alt="${original_name || original_title}"
   width="395" height="574" class="search_film_img"/>
 <div class="wrap">
   <div class="search_film_wrap">
     <p class="search_film_title">${original_name || original_title}</p>
-    <p class="search_film_genre"> | ${year}</p>
+    <p class="search_film_genre">${genre} | ${year}</p>
     <p class="stars is-hidden">${vote_average}</p>
   </div>
   <button type="button" class="js_add_collection" data-id="${id}">Add to collection</button>
@@ -47,6 +58,27 @@ function yearsFilm(release_date, first_air_date) {
       ? first_air_date.split('-')[0]
       : '';
   return year;
+}
+
+function categoriesFilms(genreIds) {
+  let categoriesFilm = [];
+  if (typeof genreIds !== 'undefined') {
+    categoriesFilm = genreIds
+      .filter(genre => typeof genre !== 'undefined')
+      .map(genre => {
+        if (!categories[genre]) {
+          return 'Film';
+        }
+        return categories[genre];
+      });
+  }
+  if (categoriesFilm.length > 2) {
+    categoriesFilm = categoriesFilm.slice(0, 2);
+  }
+  if (categoriesFilm.length === 0) {
+    return 'Film';
+  }
+  return categoriesFilm.join(', ');
 }
 
 markupFilm(favorite);
--- src/js/favorite_local.js
+++ src/js/favorite_local.js
@@ -8,28 +8,36 @@ const KEY_FAVORITE = 'favorite';
 const favoriteArr = JSON.parse(localStorage.getItem(KEY_FAVORITE)) ?? [];
 const favorite = localStorage.getItem(KEY_FAVORITE) ?? [];
 
-const jsList = document.querySelector('.js_list')
+const jsList = document.querySelector('.js_list');
 
 export async function onClickFilm(ev) {
-    let filmId = null;
-    const btn = document.querySelector('.js_add_collection')
-    if (ev.target.classList.contains('js_add_collection')) {
-      const { id } = ev.target.closest('li').dataset;
-      filmId = await findFilm(Number(id));
-      if (filmId) {
-        const inStorage = favoriteArr.some(({ id }) => id === filmId);
-        if (inStorage) {
-          return;
-        }
+  let filmId = null;
+  const btn = ev.target.closest('.js_add_collection');
+
+  if (btn) {
+    const { id } = btn.closest('li').dataset;
+    filmId = await findFilm(Number(id));
+    if (filmId) {
+      const inStorage = favoriteArr.some(({ id }) => id === filmId);
+      if (inStorage) {
+        // Видалення фільму з localStorage
+        favoriteArr.splice(
+          favoriteArr.findIndex(film => film.id === filmId),
+          1
+        );
+        localStorage.setItem(KEY_FAVORITE, JSON.stringify(favoriteArr));
+        btn.textContent = 'Add';
+      } else {
         const film = await API.getCategoriesId(filmId);
         favoriteArr.push(film);
         localStorage.setItem(KEY_FAVORITE, JSON.stringify(favoriteArr));
+        btn.textContent = 'Delete';
       }
     }
   }
-  
-  async function findFilm(ev) {
-    const res = await API.getCategoriesId(ev);
-    return res.id === ev ? res.id : null;
-  }
+}
 
+async function findFilm(ev) {
+  const res = await API.getCategoriesId(ev);
+  return res.id === ev ? res.id : null;
+}
